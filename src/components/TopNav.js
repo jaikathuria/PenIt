a2e8b46fde80a6d1588b9c66712c2bdb3cef879d
jaikathuria <jaikathuria@live.com>
2017-09-30 05:36:51 +0530

    create-react-app, bootstrap, TopNav

---
+++ src/components/TopNav.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class TopNav extends Component {
+    render () {
+        return (
+          <nav className="navbar navbar-default navbar-fixed-top">
+            <div className="container" id="top">
+              <div className="row">
+                <div className="col-xs-12">
+                  <div className="navbar-header text-center">
+                    <a className="navbar-brand brand" href="">Pen-it</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+        )
+    }
+}
